5daf1d46b62e9920a9eb89c9f84953ff7df682b6
clydiscope <clydiscope@gmail.com>
2020-10-17 17:58:10 -0400

    refracts model spec

--- spec/entry_spec.rb
+++ spec/entry_spec.rb
@@ -4,19 +4,31 @@ require_relative '../models/entry'
 
 RSpec.describe Entry do
 	describe "attitudes" do
-		it "responds to name" do
-			entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
-			expect(entry.name).to eq('Ada Lovelace')
-		end
-		
-		it "responds to phone number" do
-			entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
-			expect(entry.phone_number).to eq('010.012.1815')
-		end
-		
-		it "responds to email" do
-			entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
-			expect(entry.email).to eq('augusta.king@lovelace.com')
-		end		
+		let(:entry) { Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com') }
+			
+			it "responds to name" do
+				expect(entry.name).to eq('Ada Lovelace')
+			end
+			
+			it "reports its name" do
+				expect(entry).to respond_to(:name)
+			end
+			
+			it "responds to phone number" do
+				expect(entry.phone_number).to eq('010.012.1815')
+			end
+
+			it "reports its phone nubmer" do
+				expect(entry).to respond_to(:phone_number)
+			end
+			
+			it "responds to email" do
+				expect(entry.email).to eq('augusta.king@lovelace.com')
+			end		
+
+			it "reports its email" do
+				expect(entry).to respond_to(:email)
+			end
+
 	end
 end
